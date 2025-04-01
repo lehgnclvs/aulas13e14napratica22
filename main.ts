557743056bb79ca6dbc2b5ceebd7be5797697b1d
Leticia 1C <leticiafabianags@gmail.com>
2025-04-01 09:21:41 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,73 @@
+basic.forever(function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # . # . .
+        . # . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # # # # .
+        # # . . .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # .
+        . . # . #
+        . . . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . .
+        . # # # #
+        . . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # # .
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        # # # # .
+        . . # . .
+        . # . # .
+        # . . # .
+        `)
+})
